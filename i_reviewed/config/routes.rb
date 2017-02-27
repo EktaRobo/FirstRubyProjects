ea721bd77e93c07551211ff711c2dc2ae41c09d4
Ekta K <diego.developers@robosftin.com>
2017-02-27 10:19:55 +0530

    trying to deploy

--- i_reviewed/config/routes.rb
+++ i_reviewed/config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books do
     resources :notes, only: [:create, :destroy]
   end
   root to: "books#index"
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
 end
